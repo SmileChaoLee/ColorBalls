31a5aefeaca725265bd47d4f9bf3cedab8fa91f4
SmileChaoLee <leechao1959@gmail.com>
2025-11-08 00:56:08 -0500

    Fixed bugs: crashing because SoundPoolUtil.soundPool is null when rotating
    more one release()s were called because multiple inheritances

--- smilelibraries/src/main/java/com/smile/smilelibraries/utilities/SoundPoolUtil.java
+++ smilelibraries/src/main/java/com/smile/smilelibraries/utilities/SoundPoolUtil.java
@@ -2,27 +2,37 @@ package com.smile.smilelibraries.utilities;
 
 import android.content.Context;
 import android.media.SoundPool;
+import android.util.Log;
 
 public class SoundPoolUtil {
 
+    private final String mTAG = "SoundPoolUtil";
     private SoundPool soundPool;
-    private int soundId;
+    private final int soundId;
 
-    @SuppressWarnings("deprecation")
     public SoundPoolUtil(final Context context, final int rawId) {
+        Log.d(mTAG, "SoundPoolUtil");
         int maxStreams = 1;
         soundPool = new SoundPool.Builder()
                 .setMaxStreams(maxStreams)
                 .build();
         soundId = soundPool.load(context, rawId, 1);
+        Log.d(mTAG, "SoundPoolUtil.soundId.soundPool = " + soundPool);
     }
 
     public void playSound() {
-        soundPool.play(soundId, 1,1, 1, 0, 1f);
+        Log.d(mTAG, "playSound.soundPool = " + soundPool);
+        if (soundPool != null) {
+            soundPool.play(soundId, 1, 1,
+                    1, 0, 1f);
+        }
     }
 
     public void release() {
-        soundPool.release();
-        soundPool = null;
+        Log.d(mTAG, "release.soundPool = " + soundPool);
+        if (soundPool != null) {
+            soundPool.release();
+            soundPool = null;
+        }
     }
 }
