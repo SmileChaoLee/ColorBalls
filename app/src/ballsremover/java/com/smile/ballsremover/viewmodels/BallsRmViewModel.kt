0f2950f45639f57ba37281c7274e34f42ba2c6d4
SmileChaoLee <leechao1959@gmail.com>
2026-06-15 17:30:04 -0400

    Change WhichGame.REMOVE_BALLS to getWhichGame()

--- app/src/ballsremover/java/com/smile/ballsremover/viewmodels/BallsRmViewModel.kt
+++ app/src/ballsremover/java/com/smile/ballsremover/viewmodels/BallsRmViewModel.kt
@@ -99,7 +99,7 @@ class BallsRmViewModel(private val bRmPresenter: BallsRmPresenter)
 
     private fun initData() {
         LogUtil.i(TAG, "initData")
-        brGameProp.initializeKeepSetting(WhichGame.REMOVE_BALLS)
+        brGameProp.initializeKeepSetting(getWhichGame())
         brGridData.initialize()
     }
 
