6bc3131d07003f82bc2f6cc446ad0d0611cb36bf
SmileChaoLee <leechao1959@gmail.com>
2026-06-18 13:07:30 -0400

    Finished AdMob settings

--- app/src/reversi/java/com/smile/reversi/ReversiApp.kt
+++ app/src/reversi/java/com/smile/reversi/ReversiApp.kt
@@ -22,19 +22,14 @@ class ReversiApp : BaseApp() {
     }
 
     override fun getInterstitial(): AdMobInterstitial? {
-        val admobInterstitialID = "ca-app-pub-8354869049759576/6690798717"
-        // not interstitial ad for now
         // return AdMobInterstitial(applicationContext, admobInterstitialID)
         return null
     }
 
     companion object {
-        // const val META_BANNER_ID = "200699663911258_423008208347068"
-        // const val META_BANNER_ID2 = "200699663911258_619846328663254"
-        // const val META_INTERSTITIAL_ID = "200699663911258_200701030577788" // for colorballs
         // Google AdMob
-        const val ADMOB_BANNER_ID = "ca-app-pub-8354869049759576/7152164841"
-        const val ADMOB_BANNER_ID2 = "ca-app-pub-8354869049759576/7343399477"
-        const val ADMOB_NATIVE_ID = "ca-app-pub-8354869049759576/3429645905"
+        const val ADMOB_BANNER_ID = "ca-app-pub-8354869049759576/4457337489"
+        const val ADMOB_BANNER_ID2 = "ca-app-pub-8354869049759576/5988131333"
+        const val ADMOB_NATIVE_ID = "ca-app-pub-8354869049759576/6305871708"
     }
 }
